1ef8cb6b5e91137b01add44e8cc94cbf9400cc94
Mejbah <Mejbah@DESKTOP-FKS50IU>
2017-11-05 14:32:02 -0500

    new updated

---
+++ project2 github/src/project2github.java
@@ -0,0 +1,14 @@
+
+public class project2github {
+
+	public static void main(String[] args) {
+		
+		String[] x = {"dhaka", "NYC", "Vienna"};
+		for (int i=0; i<=x.length;i++){
+		if 
+		(x[i]=="dhaka")
+				System.out.println("dhaka is available");
+
+	}
+	}
+}
